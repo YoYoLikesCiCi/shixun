c9217e0f77a70c76a970f337fa5c2e7144f41b6b
孙起 <YoYoLikesCiCi@live.cn>
2020-11-10 11:16:32 +0800

    添加开屏图片并跳转第二屏

--- app/src/main/java/com/example/shixun/MainActivity.java
+++ app/src/main/java/com/example/shixun/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.shixun;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //延迟一秒，跳转到主页面
+        new Handler().postDelayed(()->{
+            startActivity(new Intent(MainActivity.this,MActivity.class));
+            finish();
+        },2000);
     }
 }
