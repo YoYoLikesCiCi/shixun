c9833588a2768456573066132388f66533542f15
孙起 <YoYoLikesCiCi@live.cn>
2020-11-10 17:51:26 +0800

    实训第二天任务完成

--- app/src/main/java/com/example/shixun/MActivity.java
+++ app/src/main/java/com/example/shixun/MActivity.java
@@ -3,12 +3,19 @@ package com.example.shixun;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RadioGroup;
 
 public class MActivity extends AppCompatActivity {
 
+    private RadioGroup rg_main;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_m);
+//
+        rg_main = findViewById(R.id.rg_main);
+        rg_main.check(R.id.rb_home);
     }
+
+
 }
